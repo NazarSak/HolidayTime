fe46047b7d7728f19f32a34456b17650323b6322
NazarSak <112251832+NazarSak@users.noreply.github.com>
2024-01-24 21:59:11 +0200

    ura make

--- HolidayTime/src/Components/slider/Slider.jsx
+++ HolidayTime/src/Components/slider/Slider.jsx
@@ -1,33 +1,50 @@
-import PropTypes from 'prop-types';
-import { Swiper, SwiperSlide } from "swiper/react";
-import 'swiper/swiper.min.css';
-// import "./swiperStyle.css"; 
+import { useState } from "react";
+import ArrayHouses from "../../Helpers/ArrayHouses";
+import { Section,SectionCon } from "./swiper.styled";
+import ArrowLeft from "../../assets/SVG/arrowLeft.svg";
+import './swiper.css'
 
-const Slider = ({ ArrayHouses }) => {
+
+const Slider = () => {
+  const [houses, setHouses] = useState(ArrayHouses);
+  const [index, setIndex] = useState(0);
+
+  const handlePrevSlide = () => {
+    const newIndex = (index + 1) % houses.length;
+    setIndex(newIndex);
+  };
+
   return (
-    <>
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
-      >
-        {ArrayHouses.map(({ smallImg,}, index) => (
-          <SwiperSlide key={index}>
-            <img src={smallImg} alt="" />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </>
+    <Section>
+      <SectionCon>
+        {ArrayHouses.map((house, houseIndex) => {
+          const { id, smallImg, details } = house;
+
+          let position = "nextSlide";
+          if (houseIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            houseIndex === index - 1 ||
+            (index === 0 && houseIndex === houses.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+          return (
+            <article className={position} key={id}>
+              <img src={smallImg} alt="Img" className="img" />
+              <p>{details}</p>
+              <img
+                src={ArrowLeft}
+                onClick={handlePrevSlide}
+                alt=""
+              />
+            </article>
+          );
+        })}
+      </SectionCon>
+    </Section>
   );
 };
-
-Slider.propTypes = {
-    ArrayHouses: PropTypes.arrayOf(
-      PropTypes.shape({
-        smallImg: PropTypes.string.isRequired,
-      })
-    ).isRequired,
-  };
 
 export default Slider;
---
+++ HolidayTime/src/Components/slider/swiper.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+  position: relative;
+  width: 100%;
+  width: 1488px;
+  height: 604px;
+  overflow: hidden;
+`;
+
+export const SectionCon = styled.div``;
